b0d9e39eb680bb8c8a2edc47fdab86bf608da836
Davi Severo Santana <severosantana2014@gmail.com>
2022-07-12 21:23:37 -0300

    12/07

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -11,6 +11,13 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.textView)
+        nome.text = "Engates HongKong"
+        val tipo = findViewById<TextView>(R.id.tipo)
+        tipo.text = "Honnda City"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "269,00R$"
     }
+
 
 }
