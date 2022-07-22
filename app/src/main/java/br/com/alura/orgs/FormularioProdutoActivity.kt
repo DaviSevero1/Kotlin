10b91c70711b2d466a719772858a77fbda69289f
Davi Severo Santana <severosantana2014@gmail.com>
2022-07-22 16:10:57 -0300

    Att 22/07/2022

---
+++ app/src/main/java/br/com/alura/orgs/FormularioProdutoActivity.kt
@@ -0,0 +1,14 @@
+package br.com.alura.orgs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.orgs.R.*
+
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_formulario_produto)
+    }
+
+}
--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,10 +1,14 @@
 package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -18,8 +22,17 @@ class MainActivity : Activity() {
         //val valor = findViewById<TextView>(R.id.valor)
         //valor.text = "269,00R$"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(nome = "teste",
+                    descricao = "teste desc",
+                    valor = BigDecimal("19.99")
+                ),
+            Produto(nome = "teste 01",
+                    descricao = "teste desc 01",
+                    valor = BigDecimal("29.99")
+            )
+        ))
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
-
 
 }
